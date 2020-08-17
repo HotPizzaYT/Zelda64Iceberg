c5e38ef0a170a5653fc773504d394ae8ca42cb5a
HotPizzaYT <57576298+HotPizzaYT@users.noreply.github.com>
2020-08-17 06:52:24 -0500

    Add all files

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<meta content="Explaination of the Zelda 64 iceberg" name="description" />
+<meta name="image" content="z64ice.png"/>
+<title>The Zelda 64 iceberg explaination</title>
+<style>
+body {
+text-align: center;
+font-family: sans-serif;
+}
+</style>
+</head>
+<body>
+<h1>The Zelda 64 iceberg</h1>
+<br>
+<p><?php include("counter.php") ?></p>
+<p>Thanks for 80 views</p>
+<img src="z64ice.png" width="250" height="500" title="Original image by ChrisCube, updated by Error0x404" />
+<br>
+There are many mysteries surrounding The Legend of Zelda, Ocarina of Time. One of them happens to be that every copy on the 64DD is personalizeds<br>
+<p>Why do I say this?</p>
+Well, it's not very known but Nintendo had made a partnership with Don't Ask Software in 1997 to create an AI of a previously deceased Nintendo employee.
+<br> Ever since then, every official copy of Ocarina of Time for the 64DD has been cursed.
+<br>
+<br>
+There are anomalous areas such as the <b>sewer</b> which is only accessible in personalized copies through playing Zelda's Lullaby in front of the fetuses in the Lakeside Laboratory.
+<br>
+I don't reccommend going there as it can cause stroke like symptoms and various mental illnesses. If you do, however, manage to find the triforce in the sewer the game will present you with an endscreen, similar to Super Mario 64's
+<br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/pBOD9CsqUb8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<br>
+The endscreen includes text that reads "You were never meant to see this but..." and "The End Nintendo 1998". Along with this is a glowing screenshot of the titlescreen with text added at the bottom by the AI.
+<br>
+<br>
+Here is the sample of the sewer music from my personalized copy:
+<br>
+<audio controls src="SewerTambourine.ogg">Audio tag not supported, <a href="SewerTambourine.ogg">Click here</a> to download.</audio>
+<br>
+<br>
+You may also download the .it file with the soundfont already applied <a href="sewer.it">here</a>
+
+</body>
+</html>
